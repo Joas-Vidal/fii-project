b1101c517853d8f09db930426b1f30843baabf96
Josue Vidal <josuex120@hotmail.com>
2024-05-20 21:25:59 -0300

    fix redirecionamento

--- controller/login.php
+++ controller/login.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($senha = $row['senha']) {
             // Senha correta, login bem-sucedido
             // Redirecionar para index.php
-            header("Location: ../index.php");
+            header("Location: ../home.php");
             exit; // Importante: encerrar o script após o redirecionamento
         } else {
             // Senha incorreta
